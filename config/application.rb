25997ccdd1fc9775db8fc432f6d00550029ad2b9
ke-me <fukudakei@fukudatkonpyuta.lan>
2024-10-05 17:31:06 +0900

    デプロイ設定

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,8 @@ module WeatherBot
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    #
+    ## デプロイするサービスのホストを追加する
+    config.hosts << 'xxx.onrender.com'
   end
 end
